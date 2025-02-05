cd2e648e08f0f51dd4afc75eb712bf92bee72e5f
miten <57693631+m1ten@users.noreply.github.com>
2025-02-05 16:07:52 -0500

    minimum set to ventura

--- autoclicker.rb
+++ autoclicker.rb
@@ -5,8 +5,7 @@ cask "AutoClicker" do
   version "1.7.28"
   sha256 "2ade6ccabcf0f8c8a0bef51eb81334f6bbb9a87373a7298e314d91d1bae0cf9b"
 
-  url "https://github.com/othyn/macos-auto-clicker/releases/download/v#{version}/AutoClicker.zip",
-    verified: "github.com/othyn/macos-auto-clicker/"
+  url "https://github.com/othyn/macos-auto-clicker/releases/download/v#{version}/AutoClicker.zip"
   name "AutoClicker"
   desc "A simple auto clicker for macOS in swift"
   homepage "https://github.com/othyn/macos-auto-clicker"
@@ -17,7 +16,7 @@ cask "AutoClicker" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :ventura"
 
   app "AutoClicker.app"
 
