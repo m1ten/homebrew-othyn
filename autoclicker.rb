ae76134796d5634887b72625bdc306c97c4478bb
miten <57693631+m1ten@users.noreply.github.com>
2025-02-05 15:53:17 -0500

    autoclicker

---
+++ autoclicker.rb
@@ -0,0 +1,28 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "autoclicker" do
+  version "1.7.28"
+  sha256 "2ade6ccabcf0f8c8a0bef51eb81334f6bbb9a87373a7298e314d91d1bae0cf9b"
+
+  url "https://github.com/othyn/macos-auto-clicker/releases/download/v#{version}/AutoClicker.zip",
+    verified: "github.com/othyn/macos-auto-clicker/"
+  name "AutoClicker"
+  desc "A simple auto clicker for macOS in swift"
+  homepage "https://github.com/othyn/macos-auto-clicker"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "AutoClicker.app"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  zap trash: [
+    "~/Library/Preferences/com.othyn.auto-clicker.plist",
+  ]
+end
